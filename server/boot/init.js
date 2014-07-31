2e5518475c4553f6c14594e3afe78b51f20589b3
Bram Borggreve <borggreve@gmail.com>
2014-07-31 13:51:05 +0200

    Adds admin to admin role, adds user@user.com / user

--- server/boot/init.js
+++ server/boot/init.js
@@ -1,24 +1,45 @@
-
+'use strict';
 
 module.exports = function(app) {
 
-  if (app.dataSources.db.name !== 'Memory') return;
+  if (app.dataSources.db.name !== 'Memory') { return; }
 
   console.error('Started creating inital data.');
 
   var User = app.models.User;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
 
 
   User.create({
-    email: 'admin@admin.com',   // required by default
-    password: 'admin'        // required by default
+    email: 'admin@admin.com',
+    firstName: 'System',
+    lastName: 'Admin',
+    password: 'admin'
   }, function (err, user) {
 
-    console.log('err', err);
-    console.log('user', user);
+    if(err) { console.log('err', err); }
+
+     Role.create({name: 'admin'}, function (err, role) {
+       role.principals.create({principalType: RoleMapping.USER, principalId: user.id});
+     });
 
   });
 
+  User.create({
+    email: 'user@user.com',
+    firstName: 'App',
+    lastName: 'User',
+    password: 'user'
+  }, function (err, user) {
+
+    if(err) { console.log('err', err); }
+
+     Role.create({name: 'admin'}, function (err, role) {
+       role.principals.create({principalType: RoleMapping.USER, principalId: user.id});
+     });
+
+  });
 
 
 };
