5492a17a0136404b0ed5a7beb3a7328499c3e7e4
Bram Borggreve <borggreve@gmail.com>
2014-08-12 14:10:36 +0200

    Adds counters to dashboard

--- client/app/scripts/controllers/home.js
+++ client/app/scripts/controllers/home.js
@@ -11,13 +11,36 @@ angular.module('loopbackApp')
   .config(function($stateProvider) {
     $stateProvider.state('app.home', {
       url: '',
-      templateUrl: 'views/home.html'
+      templateUrl: 'views/home.html',
+      controller: 'HomeCtrl'
     });
   })
-  .controller('HomeCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('HomeCtrl', function($scope, $http, Post, Product) {
+
+    $scope.count = {};
+
+    Post.find(function(posts) {
+      $scope.count.posts = posts.length;
+    });
+
+    Product.find(function(products) {
+      $scope.count.products = products.length;
+    });
+
+    $scope.count.events = 0;
+    $scope.count.files = 0;
+
+
+    $http.get('http://localhost:3000/api/containers/container1/files').success(function(data) {
+      $scope.count.files = data.length;
+    });
+
+    // Event.find(function(events){
+    // $scope.count.events = events.length;
+    // });
+
+    // Post.find(function(posts){
+    //   $scope.count.posts = posts.length;
+    // });
+
   });
