add88c4f33e62d58d7b06144dee12d93b3213c9e
Bram Borggreve <borggreve@gmail.com>
2014-10-19 01:01:24 +0200

    Update editor theme

--- client/app/scripts/controllers/pages.js
+++ client/app/scripts/controllers/pages.js
@@ -8,82 +8,88 @@
  * Controller of the loopbackApp
  */
 angular.module('loopbackApp')
-  .config(function($stateProvider) {
+  .config(function ($stateProvider) {
     $stateProvider.state('app.pages', {
       abstract: true,
       url: '/pages',
       templateUrl: 'views/pages/main.html',
       controller: 'PagesCtrl'
     })
-    .state('app.pages.list', {
-      url: '',
-      templateUrl: 'views/pages/list.html',
-      controller: 'PagesCtrl'
-    })
-    .state('app.pages.add', {
-      url: '/add',
-      templateUrl: 'views/pages/form.html',
-      controller: 'PagesCtrl'
-    })
-    .state('app.pages.edit', {
-      url: '/:id/edit',
-      templateUrl: 'views/pages/form.html',
-      controller: 'PagesCtrl'
-    })
-    .state('app.pages.view', {
-      url: '/:id',
-      templateUrl: 'views/pages/view.html',
-      controller: 'PagesCtrl'
-    });
+      .state('app.pages.list', {
+        url: '',
+        templateUrl: 'views/pages/list.html',
+        controller: 'PagesCtrl'
+      })
+      .state('app.pages.add', {
+        url: '/add',
+        templateUrl: 'views/pages/form.html',
+        controller: 'PagesCtrl'
+      })
+      .state('app.pages.edit', {
+        url: '/:id/edit',
+        templateUrl: 'views/pages/form.html',
+        controller: 'PagesCtrl'
+      })
+      .state('app.pages.view', {
+        url: '/:id',
+        templateUrl: 'views/pages/view.html',
+        controller: 'PagesCtrl'
+      });
   })
 
-.controller('PagesCtrl', function($scope, $state, $stateParams, toasty, Page) {
+  .controller('PagesCtrl', function ($scope, $state, $stateParams, toasty, Page) {
 
-  var pageId = $stateParams.id;
+    var pageId = $stateParams.id;
 
-  if (pageId) {
-    $scope.page = Page.findById({
-      id: pageId
-    }, function() {}, function(err) {
-      console.log(err);
-    });
-  } else {
-    $scope.page = {'content': '# Start typing here!'};
-  }
+    if (pageId) {
+      $scope.page = Page.findById({
+        id: pageId
+      }, function () {
+      }, function (err) {
+        console.log(err);
+      });
+    } else {
+      $scope.page = {'content': '# Start typing here!'};
+    }
 
-  function loadPages() {
-    $scope.pages = Page.find();
-  }
+    function loadPages() {
+      $scope.pages = Page.find();
+    }
 
-  loadPages();
+    loadPages();
 
-  $scope.delete = function(id) {
-    // if (confirm('Are you sure?') === false) {
+    $scope.delete = function (id) {
+      // if (confirm('Are you sure?') === false) {
       // return false;
-    // }
-    Page.deleteById(id, function() {
-      toasty.pop.success({title: 'Page deleted', msg: 'Your page is deleted!', sound: false});
-      loadPages();
-      $state.go('app.pages.list');
-      console.log();
-    }, function(err) {
-      toasty.pop.error({title: 'Error deleting page', msg: 'Your page is not deleted: ' + err, sound: false});
-    });
+      // }
+      Page.deleteById(id, function () {
+        toasty.pop.success({title: 'Page deleted', msg: 'Your page is deleted!', sound: false});
+        loadPages();
+        $state.go('app.pages.list');
+        console.log();
+      }, function (err) {
+        toasty.pop.error({title: 'Error deleting page', msg: 'Your page is not deleted: ' + err, sound: false});
+      });
 
-  };
+    };
 
-  $scope.formFields = [{
-    key: 'name',
-    label: 'Name',
-    type: 'text',
-    required: true
-  }, {
-    key: 'slug',
-    label: 'Slug',
-    type: 'text'
-  }];
-
-  $scope.formOptions = {
+    $scope.formFields = [
+      {
+        key: 'name',
+        label: 'Name',
+        type: 'text',
+        required: true
+      },
+      {
+        key: 'slug',
+        label: 'Slug',
+        type: 'text'
+      }
+    ];
+    $scope.editorOptions = {
+      theme: 'monokai'
+    };
+    $scope.formOptions = {
 
       //Set the id of the form
       uniqueFormId: true,
@@ -95,18 +101,18 @@ angular.module('loopbackApp')
       //Set the text on the default submit button
       //default: Submit
       submitCopy: 'Save'
-  };
+    };
 
-  $scope.onSubmit = function() {
+    $scope.onSubmit = function () {
 
-    Page.upsert($scope.page, function() {
-      toasty.pop.success({title: 'Page saved', msg: 'Your page is safe with us!', sound: false});
-      $state.go('^.list');
-    }, function(err) {
-      console.log(err);
-    });
+      Page.upsert($scope.page, function () {
+        toasty.pop.success({title: 'Page saved', msg: 'Your page is safe with us!', sound: false});
+        $state.go('^.list');
+      }, function (err) {
+        console.log(err);
+      });
 
-  };
+    };
 
 
-});
+  });
