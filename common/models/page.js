eedbd03f755ddf2234872886ee390ac4f6753c64
Bram Borggreve <borggreve@gmail.com>
2014-10-19 02:34:36 +0200

    API - Add /html method to Page model, use showdown to parse markdown to html

---
+++ common/models/page.js
@@ -0,0 +1,22 @@
+var Showdown = require('showdown');
+
+module.exports = function (Page) {
+
+  var converter = new Showdown.converter();
+
+  Page.html = function (id, cb) {
+    Page.findById(id, function (err, page) {
+      if (err) return cb(err);
+      var result = page;
+      result.html = converter.makeHtml(page.content);
+      cb(err, result);
+    });
+  };
+
+  Page.remoteMethod('html', {
+    accepts: {arg: 'id', type: 'string'},
+    returns: {arg: 'content', type: 'string'},
+    http: {path: '/html', verb: 'get'}
+  });
+
+};
