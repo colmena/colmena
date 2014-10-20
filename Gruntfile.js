cc223fa634e6bcefb99aaeb91bacc3a6adb772da
Bram Borggreve <borggreve@gmail.com>
2014-10-20 03:22:20 +0200

    Prepare for Heroku

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,7 +84,7 @@ module.exports = function (grunt) {
       },
       production: {
         options: {
-          dest: '<%= yeoman.dist %>/scripts/config.js'
+          dest: '<%= yeoman.app %>/scripts/config.js'
         },
         constants: {
           ENV: {
