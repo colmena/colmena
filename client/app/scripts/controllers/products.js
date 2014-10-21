2ca53eba9cd27b9c066289d465f1982a148e460d
Bram Borggreve <borggreve@gmail.com>
2014-10-21 08:31:19 +0200

    Implements decent Product system

--- client/app/scripts/controllers/products.js
+++ client/app/scripts/controllers/products.js
@@ -7,97 +7,165 @@
  * # ProductsCtrl
  * Controller of the clientApp
  */
-angular.module('loopbackApp')
+var app = angular.module('loopbackApp');
 
-  .config(function($stateProvider) {
-    $stateProvider.state('app.products', {
-      abstract: true,
-      url: '/products',
-      templateUrl: 'views/products/main.html',
-      controller: 'ProductsCtrl'
-    })
-    .state('app.products.list', {
-      url: '',
-      templateUrl: 'views/products/list.html',
-      controller: 'ProductsCtrl'
-    })
-    .state('app.products.add', {
-      url: '/add',
-      templateUrl: 'views/products/form.html',
-      controller: 'ProductsCtrl'
-    })
-    .state('app.products.edit', {
-      url: '/:id/edit',
-      templateUrl: 'views/products/form.html',
-      controller: 'ProductsCtrl'
-    })
-    .state('app.products.view', {
-      url: '/:id',
-      templateUrl: 'views/products/view.html',
-      controller: 'ProductsCtrl'
-    });
-  })
+app.config(function ($stateProvider) {
+  $stateProvider.state('app.products', {
+    abstract: true,
+    url: '/products',
+    templateUrl: 'views/products/main.html',
+    controller: 'ProductsCtrl'
+  }).state('app.products.list', {
+    url: '',
+    templateUrl: 'views/products/list.html',
+    controller: 'ProductsCtrl'
+  }).state('app.products.add', {
+    url: '/add/:categoryId',
+    templateUrl: 'views/products/form.html',
+    controller: 'ProductsCtrl'
+  }).state('app.products.edit', {
+    url: '/:id/edit',
+    templateUrl: 'views/products/form.html',
+    controller: 'ProductsCtrl'
+  }).state('app.products.addcategory', {
+    url: '/addcategory',
+    templateUrl: 'views/products/categoryform.html',
+    controller: 'CategoriesCtrl'
+  }).state('app.products.view', {
+    url: '/:id',
+    templateUrl: 'views/products/view.html',
+    controller: 'ProductsCtrl'
+  }).state('app.products.editcategory', {
+    url: '/editcategory/:categoryId',
+    templateUrl: 'views/products/categoryform.html',
+    controller: 'CategoriesCtrl'
+  });
+});
 
-  .controller('ProductsCtrl', function($scope, $state, $stateParams, toasty, Product) {
+app.controller('ProductsCtrl', function ($scope, $state, $stateParams, toasty, Product, Category) {
 
   var productId = $stateParams.id;
+  var categoryId = $stateParams.categoryId;
 
   if (productId) {
     $scope.product = Product.findById({
       id: productId
-    }, function() {}, function(err) {
+    }, function (product) {
+      product.category = Product.category({id: product.categoryId});
+    }, function (err) {
       console.log(err);
     });
   } else {
     $scope.product = {};
   }
 
+  if (categoryId) {
+    $scope.product.categoryId = categoryId;
+  }
+
   function loadItems() {
-    $scope.products = Product.find();
+    $scope.categories = [];
+    Category.find(function (categories) {
+      angular.forEach(categories, function (category) {
+        category.products = Category.products({id: category.id});
+        this.push(category);
+      }, $scope.categories);
+    });
   }
 
   loadItems();
 
-  $scope.delete = function(id) {
-    // if (confirm('Are you sure?') === false) {
-    //   return false;
-    // }
-    Product.deleteById(id, function() {
+  $scope.delete = function (id) {
+    Product.deleteById(id, function () {
       toasty.pop.success({title: 'Product deleted', msg: 'Your product is deleted!', sound: false});
       loadItems();
       $state.go('app.products.list');
-      console.log();
-    }, function(err) {
+    }, function (err) {
       toasty.pop.error({title: 'Error deleting product', msg: 'Your product is not deleted: ' + err, sound: false});
     });
 
   };
 
+  $scope.deletecategory = function(id) {
+    Category.deleteById(id, function() {
+      toasty.pop.success({title: 'Category deleted', msg: 'Your category is deleted!', sound: false});
+      loadItems();
+    }, function(err) {
+      toasty.pop.error({title: 'Error deleting category', msg: 'Your category is not deleted: ' + err, sound: false});
+    });
+  };
+
+  $scope.formFields = [
+    {
+      key: 'name',
+      type: 'text',
+      label: 'Name',
+      required: true
+    },
+    {
+      key: 'categoryId',
+      type: 'text',
+      label: 'Category',
+      required: true
+    },
+    {
+      key: 'price',
+      type: 'number',
+      label: 'Price'
+    }
+  ];
+
+  $scope.formOptions = {
+    uniqueFormId: true,
+    hideSubmit: false,
+    submitCopy: 'Save'
+  };
+
+  $scope.onSubmit = function () {
+    Product.upsert($scope.product, function () {
+      toasty.pop.success({title: 'Product saved', msg: 'Your product is safe with us!', sound: false});
+      $state.go('^.list');
+    }, function (err) {
+      console.log(err);
+    });
+  };
+
+});
+
+app.controller('CategoriesCtrl', function($scope, $state, $stateParams, toasty, Category) {
+  var categoryId = $stateParams.categoryId;
+  if (categoryId) {
+    $scope.category = Category.findById({
+      id: categoryId
+    }, function(category) {
+      $scope.products = Category.products({id: category.id});
+    }, function(err) {
+      console.log(err);
+    });
+  } else {
+    $scope.category = {};
+  }
+
   $scope.formFields = [{
-    key: 'title',
+    key: 'name',
     type: 'text',
-    label: 'Title',
-    required: true
-  }, {
-    key: 'body',
-    type: 'text',
-    label: 'Body',
+    label: 'Name',
     required: true
   }];
 
   $scope.formOptions = {
-      uniqueFormId: true,
-      hideSubmit: false,
-      submitCopy: 'Save'
+    uniqueFormId: true,
+    hideSubmit: false,
+    submitCopy: 'Save'
   };
 
   $scope.onSubmit = function() {
-    Product.upsert($scope.product, function() {
-      toasty.pop.success({title: 'Product saved', msg: 'Your product is safe with us!', sound: false});
+    Category.upsert($scope.category, function() {
+      toasty.pop.success({title: 'Category saved', msg: 'Your category is safe with us!', sound: false});
       $state.go('^.list');
     }, function(err) {
       console.log(err);
     });
   };
 
-  });
+});
--- server/boot/init.js
+++ server/boot/init.js
@@ -65,19 +65,47 @@ module.exports = function(app) {
   var Product = app.models.Product;
 
   Category.create({
-    name: 'General Products'
+    name: 'Beer'
   }, function(err, category) {
     if (err) {
       console.log('err', err);
     }
     Product.create({
-      name: 'Generic product',
+      name: 'Draft beer',
+      price: '250',
+      percentage: '5',
       categoryId: category.id
     }, function(err, data) {
       console.log(data);
     });
     Product.create({
-      name: 'Specific product',
+      name: 'Bottled beer',
+      price: '350',
+      percentage: '5',
+      categoryId: category.id
+    }, function(err, data) {
+      console.log(data);
+    });
+  });
+
+  Category.create({
+    name: 'Wine'
+  }, function(err, category) {
+    if (err) {
+      console.log('err', err);
+    }
+    Product.create({
+      name: 'Red wine',
+      price: '350',
+      percentage: '12',
+      categoryId: category.id
+    }, function(err, data) {
+      console.log(data);
+    });
+    Product.create({
+      name: 'White wine',
+      price: '350',
+      percentage: '12',
       categoryId: category.id
     }, function(err, data) {
       console.log(data);
