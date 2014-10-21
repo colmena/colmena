e4cbd9380fc8b6f9b9624679e19ec51b9f1eeece
Bram Borggreve <borggreve@gmail.com>
2014-10-21 05:44:48 +0200

    Fix jshint error

--- client/app/scripts/directives/admin-form.js
+++ client/app/scripts/directives/admin-form.js
@@ -11,7 +11,7 @@ angular.module('loopbackApp')
     return {
       template: '<div></div>',
       restrict: 'E',
-      link: function postLink(scope, element, attrs) {
+      link: function postLink(scope, element) {
         element.text('this is the adminForm directive');
       }
     };
