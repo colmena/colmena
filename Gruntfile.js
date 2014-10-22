78194491cdc2a37c30ed9ed538f8d2e46d39f44d
Bram Borggreve <borggreve@gmail.com>
2014-10-23 01:19:20 +0200

    Update ngmin to ng-annotate cause of warning during npm install

--- Gruntfile.js
+++ Gruntfile.js
@@ -306,10 +306,10 @@ module.exports = function (grunt) {
       }
     },
 
-    // ngmin tries to make the code safe for minification automatically by
+    // ngAnnotate tries to make the code safe for minification automatically by
     // using the Angular long form for dependency injection. It doesn't work on
     // things like resolve or inject so those have to be done manually.
-    ngmin: {
+    ngAnnotate: {
       dist: {
         files: [{
           expand: true,
@@ -454,7 +454,7 @@ module.exports = function (grunt) {
     'concurrent:dist',
     'autoprefixer',
     'concat',
-    'ngmin',
+    'ngAnnotate',
     'copy:dist',
     // 'cdnify',
     'cssmin',
