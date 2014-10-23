bd1560890e6783063096d29530c8e3c2289e722f
Bram Borggreve <borggreve@gmail.com>
2014-10-23 02:25:12 +0200

    Add Tabs to sandbox

--- client/app/scripts/controllers/sandbox.js
+++ client/app/scripts/controllers/sandbox.js
@@ -401,6 +401,19 @@ app.controller('SandboxBootstrapAlertCtrl', function ($scope) {
   };
 });
 
+app.controller('SandboxBootstrapTabsCtrl', function ($scope) {
+  $scope.tabs = [
+    { title: 'Dynamic Title 1', content: 'Dynamic content 1' },
+    { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }
+  ];
+
+  $scope.alertMe = function () {
+    setTimeout(function () {
+      alert('You\'ve selected the alert tab!');
+    });
+  };
+});
+
 app.controller('DatepickerDemoCtrl', function ($scope) {
   $scope.today = function () {
     $scope.dt = new Date();
