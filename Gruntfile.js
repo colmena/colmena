abe25c027dff131d9ab9d10c6237333bea2f8aee
Bram Borggreve <borggreve@gmail.com>
2015-01-17 04:07:53 +0700

    Enable filerev for scripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -276,7 +276,7 @@ module.exports = function (grunt) {
     filerev: {
       dist: {
         src: [
-          '<%= yeoman.dist %>/js/{,*/}*.js',
+          '<%= yeoman.dist %>/scripts/{,*/}*.js',
           '<%= yeoman.dist %>/styles/{,*/}*.css',
           '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
           '<%= yeoman.dist %>/fonts/*'
