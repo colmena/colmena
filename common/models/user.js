3a9b0ac5550a4b7dd21b611d0b4d2197738fb5ab
Bram Borggreve <borggreve@gmail.com>
2015-01-17 04:48:11 +0700

    Add ACL for users

---
+++ common/models/user.js
@@ -0,0 +1,13 @@
+module.exports = function (user) {
+
+  user.beforeCreate = function (next, user) {
+    if (user.username == undefined) {
+      user.username = user.email;
+    }
+    user.status = 'created';
+    user.created = Date.now();
+    next();
+  };
+
+
+};
