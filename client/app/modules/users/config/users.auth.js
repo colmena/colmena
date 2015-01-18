4ed5c800c68172ad2ea00323cb7c974dfefe4080
Bram Borggreve <borggreve@gmail.com>
2015-01-18 23:35:23 +0700

    Remove gettextCatalog from http interceptor

--- client/app/modules/users/config/users.auth.js
+++ client/app/modules/users/config/users.auth.js
@@ -3,7 +3,7 @@ angular.module('com.module.users')
   .config(function ($routeProvider, $httpProvider) {
 
     // Intercept 401 responses and redirect to login screen
-    $httpProvider.interceptors.push(function ($q, $location, CoreService, AppAuth, gettextCatalog) {
+    $httpProvider.interceptors.push(function ($q, $location, CoreService, AppAuth) {
       return {
         responseError: function (rejection) {
 
@@ -18,20 +18,20 @@ angular.module('com.module.users')
               if ($location.path() !== '/register') {
                 $location.path('/login');
               }
-              CoreService.toastWarning(gettextCatalog.getString('Error 401 received'), gettextCatalog.getString('We received a 401 error from the API! Redirecting to login'));
+              CoreService.toastWarning('Error 401 received', 'We received a 401 error from the API! Redirecting to login');
             }
           }
           if (rejection.status === 404) {
             console.log(rejection);
-            CoreService.toastError(gettextCatalog.getString('Error 404 received'), rejection.data.error.message);
+            CoreService.toastError('Error 404 received', rejection.data.error.message);
           }
           if (rejection.status === 422) {
             console.log(rejection);
-            CoreService.toastError(gettextCatalog.getString('Error 422 received'), rejection.data.error.message);
+            CoreService.toastError('Error 422 received', rejection.data.error.message);
           }
           if (rejection.status === 0) {
             $location.path('/');
-            CoreService.toastError(gettextCatalog.getString('Connection Refused'), gettextCatalog.getString('The connection to the API is refused. Please verify that the API is running!'));
+            CoreService.toastError('Connection Refused', 'The connection to the API is refused. Please verify that the API is running!');
           }
           return $q.reject(rejection);
         }
