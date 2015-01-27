2b30acaaec061fcd7fe3fd5f7e3e6594466a5fe9
Willian Ribeiro Angelo <agfoccus@gmail.com>
2015-01-27 15:52:44 -0200

    Module Chat with Socket.io

--- client/app/modules/chat/controllers/chat.ctrl.js
+++ client/app/modules/chat/controllers/chat.ctrl.js
@@ -1,55 +1,33 @@
 'use strict';
-var app = angular.module('com.module.chat');
+angular.module ('com.module.chat')
+  .controller ('ChatCtrl', function ($scope, User, Chat) {
 
-app.controller('ChatCtrl', function ($scope, $location, $timeout, $anchorScroll, User, Chat) {
-
-  $scope.user = User.getCurrent();
-  $scope.users = User.find();
+  // Get current User
+  $scope.user = User.getCurrent ();
   $scope.chat = {};
   $scope.messages = [];
 
-  $scope.connected = false;
-
-  $timeout(function () {
-    $scope.connected = Chat.socket.connected;
-  }, 1000);
-
-
-  console.log(Chat);
-
   // Connect Socket.io
-  Chat.on('chat message', function (msg) {
-    $scope.messages.push(msg);
+  Chat.on ('chat message', function (msg) {
+    $scope.messages.push (msg);
   });
 
   // Send Button
   $scope.sendMessage = function (inputMessage) {
-
+    var data = new Date();
     // Format object before send socket.io
     $scope.chat = {
-      user: $scope.user,
-      msg: inputMessage,
-      timestamp: Date.now()
+      firstName: $scope.user.fistName ,
+      email: $scope.user.email,
+      msg: inputMessage
     };
 
     // Send to Server
-    Chat.emit('chat message', $scope.chat);
+    Chat.emit ('chat message', $scope.chat);
     //Clean Input
     $scope.inputMessage = '';
     // Focus Input
-    document.getElementById('message').focus();
-
-    // Scroll to bottom
-    $location.hash('bottom');
-    $anchorScroll();
-  };
-
-  // Triggerd on pressing return
-  $scope.keypressCallback = function ($event) {
-    $event.preventDefault();
-    if ($scope.inputMessage !== '') {
-      $scope.sendMessage($scope.inputMessage);
-    }
+    document.getElementById("message").focus();
   };
 
 });
