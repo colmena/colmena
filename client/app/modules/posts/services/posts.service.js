cecfda9657c95da7ae86614f093f7595474f080f
Bram Borggreve <borggreve@gmail.com>
2015-02-09 00:30:11 +0100

    Clean up PostsService

--- client/app/modules/posts/services/posts.service.js
+++ client/app/modules/posts/services/posts.service.js
@@ -1,18 +1,10 @@
 'use strict';
 var app = angular.module('com.module.posts');
 
-app.service('PostsService', ['$http', 'Post', function ($http, Post) {
-  this.savePost = function (goat) {
-    return $http.post('/goats', goat);
-  };
+app.service('PostsService', ['Post', function (Post) {
 
   this.getPosts = function () {
     return Post.find().$promise;
   };
 
-  this.getPost = function (postId) {
-    return Post.findById({
-      id: postId
-    });
-  };
 }]);
