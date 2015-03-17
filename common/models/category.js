5390692d0e9092f22dbc1b244682083ce363907d
Bram Borggreve <borggreve@gmail.com>
2015-03-18 00:45:26 +0100

    Delete products if parent category is deleted

---
+++ common/models/category.js
@@ -0,0 +1,18 @@
+module.exports = function (Category) {
+
+  Category.observe('before delete', function (ctx, next) {
+
+    var Product = ctx.Model.app.models.Product;
+    Product.find({
+      where: {
+        categoryId: ctx.where.id
+      }
+    }, function (err, products) {
+      products.forEach(function (product) {
+        Product.destroyById(product.id);
+      });
+    });
+    next();
+  });
+
+};
