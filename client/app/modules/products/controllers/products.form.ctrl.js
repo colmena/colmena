09a097f81d974b166f787a1e0cdf7e447671a834
Bram Borggreve <borggreve@gmail.com>
2015-07-03 04:02:21 +0200

    Refactor products module to use schemaform

---
+++ client/app/modules/products/controllers/products.form.ctrl.js
@@ -0,0 +1,66 @@
+'use strict';
+var app = angular.module('com.module.products');
+
+function ProductsFormCtrl($state, CoreService, Product, gettextCatalog,
+                          categories, product) {
+
+  var self = this;
+
+  this.product = product;
+
+  this.schema = {
+    type: 'object',
+    title: 'Product',
+    properties: {
+      name: {
+        title: gettextCatalog.getString('Name'),
+        type: 'string'
+      },
+      categoryId: {
+        title: gettextCatalog.getString('Category'),
+        type: 'string',
+        format: 'uiselect',
+        items: categories.map(function (category) {
+          return {
+            value: category.id.toString(),
+            name: category.name
+          };
+        })
+      },
+      description: {
+        title: gettextCatalog.getString('Description'),
+        type: 'string'
+      },
+      price: {
+        title: gettextCatalog.getString('Price'),
+        type: 'string'
+      }
+    },
+    required: ['name', 'categoryId']
+  };
+
+  this.form = [
+    'name',
+    'categoryId',
+    'description',
+    'price',
+    {
+      type: 'submit',
+      title: 'OK'
+    }
+  ];
+
+  this.onSubmit = function () {
+
+    Product.upsert(self.product, function () {
+      CoreService.toastSuccess(gettextCatalog.getString(
+        'Product saved'), gettextCatalog.getString(
+        'Your product is safe with us!'));
+      $state.go('^.list');
+    }, function (err) {
+      console.log(err);
+    });
+  };
+}
+
+app.controller('ProductsFormCtrl', ProductsFormCtrl);
