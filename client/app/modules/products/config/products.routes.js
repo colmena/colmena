b3c635053b3b6327cb8fe6a4afecc6d0ae6c3bab
Bram Borggreve <borggreve@gmail.com>
2015-09-07 23:30:46 +0200

    Refactor Products module

--- client/app/modules/products/config/products.routes.js
+++ client/app/modules/products/config/products.routes.js
@@ -10,16 +10,34 @@ angular.module('com.module.products')
       .state('app.products.list', {
         url: '',
         templateUrl: 'modules/products/views/list.html',
-        controller: 'ProductsCtrl'
+        controllerAs: 'ctrl',
+        controller: function (categories) {
+          this.categories = categories;
+        },
+        resolve: {
+          categories: ['CategoriesService', function (CategoriesService) {
+            return CategoriesService.getCategories();
+          }]
+        }
       })
       .state('app.products.add', {
         url: '/add/:categoryId',
         templateUrl: 'modules/products/views/form.html',
-        controller: 'ProductsFormCtrl',
         controllerAs: 'ctrl',
+        controller: function ($state, ProductsService, categories, product) {
+          this.categories = categories;
+          this.product = product;
+          this.formFields = ProductsService.getFormFields(categories);
+          this.formOptions = {};
+          this.submit = function () {
+            ProductsService.upsertProduct(this.product).then(function () {
+              $state.go('^.list');
+            });
+          };
+        },
         resolve: {
-          categories: function (Category) {
-            return Category.find().$promise;
+          categories: function (CategoriesService) {
+            return CategoriesService.getCategories();
           },
           product: function ($stateParams) {
             return {
@@ -31,38 +49,111 @@ angular.module('com.module.products')
       .state('app.products.edit', {
         url: '/:productId/edit',
         templateUrl: 'modules/products/views/form.html',
-        controller: 'ProductsFormCtrl',
         controllerAs: 'ctrl',
+        controller: function ($state, ProductsService, categories, product) {
+          this.categories = categories;
+          this.product = product;
+          this.formFields = ProductsService.getFormFields(categories);
+          this.formOptions = {};
+          this.submit = function () {
+            ProductsService.upsertProduct(this.product).then(function () {
+              $state.go('^.list');
+            });
+          };
+        },
         resolve: {
-          categories: function (Category) {
-            return Category.find().$promise;
+          categories: function (CategoriesService) {
+            return CategoriesService.getCategories();
           },
-          product: function ($stateParams, Product) {
-            return Product.findById({id: $stateParams.productId}).$promise;
+          product: function ($stateParams, ProductsService) {
+            return ProductsService.getProduct($stateParams.productId);
           }
         }
       })
       .state('app.products.addcategory', {
         url: '/addcategory',
         templateUrl: 'modules/products/views/categoryform.html',
-        controller: 'CategoriesCtrl'
+        controllerAs: 'ctrl',
+        controller: function ($state, CategoriesService, category) {
+          this.category = category;
+          this.formFields = CategoriesService.getFormFields();
+          this.formOptions = {};
+          this.submit = function () {
+            CategoriesService.upsertCategory(this.category).then(function () {
+              $state.go('^.list');
+            });
+          };
+        },
+        resolve: {
+          category: function () {
+            return {};
+          }
+        }
       })
       .state('app.products.view', {
         url: '/:productId',
         templateUrl: 'modules/products/views/view.html',
-        resolve: {
-          product: function ($stateParams, Product) {
-            return Product.findById({id: $stateParams.productId}).$promise;
-          }
-        },
+        controllerAs: 'ctrl',
         controller: function (product) {
           this.product = product;
+          console.log(product);
         },
-        controllerAs: 'ctrl'
+        resolve: {
+          product: function ($stateParams, ProductsService) {
+            return ProductsService.getProduct($stateParams.productId);
+          }
+        }
       })
       .state('app.products.editcategory', {
         url: '/editcategory/:categoryId',
         templateUrl: 'modules/products/views/categoryform.html',
-        controller: 'CategoriesCtrl'
+        controllerAs: 'ctrl',
+        controller: function ($state, CategoriesService, category) {
+          this.category = category;
+          this.formFields = CategoriesService.getFormFields();
+          this.formOptions = {};
+          this.submit = function () {
+            CategoriesService.upsertCategory(this.category).then(function () {
+              $state.go('^.list');
+            });
+          };
+        },
+        resolve: {
+          category: function ($stateParams, CategoriesService) {
+            return CategoriesService.getCategory($stateParams.categoryId);
+          }
+        }
+      }).state('app.products.deletecategory', {
+        url: '/category/:categoryId/delete',
+        template: '',
+        controllerAs: 'ctrl',
+        controller: function ($state, CategoriesService, product) {
+          CategoriesService.deleteCategory(product.id, function () {
+            $state.go('^.list');
+          }, function () {
+            $state.go('^.list');
+          });
+        },
+        resolve: {
+          product: function ($stateParams, CategoriesService) {
+            return CategoriesService.getCategory($stateParams.categoryId);
+          }
+        }
+      }).state('app.products.delete', {
+        url: '/:productId/delete',
+        template: '',
+        controllerAs: 'ctrl',
+        controller: function ($state, ProductsService, product) {
+          ProductsService.deleteProduct(product.id, function () {
+            $state.go('^.list');
+          }, function () {
+            $state.go('^.list');
+          });
+        },
+        resolve: {
+          product: function ($stateParams, ProductsService) {
+            return ProductsService.getProduct($stateParams.productId);
+          }
+        }
       });
   });
