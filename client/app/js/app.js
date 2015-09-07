c0189124518cc453c4e892cc8db55e176d07b3a5
Bram Borggreve <borggreve@gmail.com>
2015-09-07 14:17:19 +0200

    Restructure app.js

--- client/app/js/app.js
+++ client/app/js/app.js
@@ -7,99 +7,99 @@
  *
  * Main module of the application.
  */
-angular.module('loopbackApp', [
-    'angular-loading-bar',
-    'angular.filter',
-    'angularBootstrapNavTree',
-    'angularFileUpload',
-    'btford.markdown',
-    'oitozero.ngSweetAlert',
-    'config',
-    'formly',
-    'formlyBootstrap',
-    'lbServices',
-    'monospaced.elastic',
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'ui.bootstrap',
-    'ui.codemirror',
-    'ui.gravatar',
-    'ui.grid',
-    'ui.router',
-    'toasty',
-    'autofields',
-    'gettext',
-    'angular-underscore/filters',
-    'schemaForm',
-    'ui.select',
-    'com.module.core',
-    'com.module.about',
-    'com.module.events',
-    'com.module.files',
-    'com.module.notes',
-    'com.module.pages',
-    'com.module.posts',
-    'com.module.products',
-    'com.module.sandbox',
-    'com.module.settings',
-    'com.module.users'
-  ])
-  .run(function($rootScope, $cookies, gettextCatalog) {
+var app = angular.module('loopbackApp', [
+  'angular-loading-bar',
+  'angular.filter',
+  'angularBootstrapNavTree',
+  'angularFileUpload',
+  'btford.markdown',
+  'oitozero.ngSweetAlert',
+  'config',
+  'formly',
+  'formlyBootstrap',
+  'lbServices',
+  'monospaced.elastic',
+  'ngAnimate',
+  'ngCookies',
+  'ngResource',
+  'ngRoute',
+  'ngSanitize',
+  'ngTouch',
+  'ui.bootstrap',
+  'ui.codemirror',
+  'ui.gravatar',
+  'ui.grid',
+  'ui.router',
+  'toasty',
+  'autofields',
+  'gettext',
+  'angular-underscore/filters',
+  'schemaForm',
+  'ui.select',
+  'com.module.core',
+  'com.module.about',
+  'com.module.events',
+  'com.module.files',
+  'com.module.notes',
+  'com.module.pages',
+  'com.module.posts',
+  'com.module.products',
+  'com.module.sandbox',
+  'com.module.settings',
+  'com.module.users'
+]);
 
-    $rootScope.locales = {
+app.run(function ($rootScope, $cookies, gettextCatalog) {
 
-      'de': {
-        lang: 'de',
-        country: 'DE',
-        name: gettextCatalog.getString('German')
-      },
-      'en': {
-        lang: 'en',
-        country: 'US',
-        name: gettextCatalog.getString('English')
-      },
-      'es_MX': {
-        lang: 'es_MX',
-        country: 'MX',
-        name: gettextCatalog.getString('Spanish')
-      },
-      'fr': {
-        lang: 'fr',
-        country: 'FR',
-        name: gettextCatalog.getString('Français')
-      },
-      'nl': {
-        lang: 'nl',
-        country: 'NL',
-        name: gettextCatalog.getString('Dutch')
-      },
-      'pt-BR': {
-        lang: 'pt_BR',
-        country: 'BR',
-        name: gettextCatalog.getString('Portuguese Brazil')
-      },
-      'ru_RU': {
-        lang: 'ru_RU',
-        country: 'RU',
-        name: gettextCatalog.getString('Russian')
-      }
+  $rootScope.locales = {
+    'de': {
+      lang: 'de',
+      country: 'DE',
+      name: gettextCatalog.getString('German')
+    },
+    'en': {
+      lang: 'en',
+      country: 'US',
+      name: gettextCatalog.getString('English')
+    },
+    'es_MX': {
+      lang: 'es_MX',
+      country: 'MX',
+      name: gettextCatalog.getString('Spanish')
+    },
+    'fr': {
+      lang: 'fr',
+      country: 'FR',
+      name: gettextCatalog.getString('Français')
+    },
+    'nl': {
+      lang: 'nl',
+      country: 'NL',
+      name: gettextCatalog.getString('Dutch')
+    },
+    'pt-BR': {
+      lang: 'pt_BR',
+      country: 'BR',
+      name: gettextCatalog.getString('Portuguese Brazil')
+    },
+    'ru_RU': {
+      lang: 'ru_RU',
+      country: 'RU',
+      name: gettextCatalog.getString('Russian')
     }
+  };
 
-    var lang = $cookies.lang || navigator.language || navigator.userLanguage;
+  var lang = $cookies.lang || navigator.language || navigator.userLanguage;
 
+  $rootScope.locale = $rootScope.locales[lang];
+
+  if ($rootScope.locale === undefined) {
     $rootScope.locale = $rootScope.locales[lang];
-
     if ($rootScope.locale === undefined) {
-      $rootScope.locale = $rootScope.locales[lang];
-      if ($rootScope.locale === undefined) {
-        $rootScope.locale = $rootScope.locales['en'];
-      }
+      $rootScope.locale = $rootScope.locales['en'];
     }
+  }
 
-    gettextCatalog.setCurrentLanguage($rootScope.locale.lang);
+  gettextCatalog.setCurrentLanguage($rootScope.locale.lang);
 
-  });
+});
