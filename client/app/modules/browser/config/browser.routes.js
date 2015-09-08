271eb0bf9e957e1e5ff2bd6673e7643b7333a5a2
Bram Borggreve <borggreve@gmail.com>
2015-09-08 03:30:54 +0200

    Add Model info screen

--- client/app/modules/browser/config/browser.routes.js
+++ client/app/modules/browser/config/browser.routes.js
@@ -17,7 +17,23 @@ app.config(function ($stateProvider) {
     ],
     resolve: {
       models: ['Meta', function (Meta) {
-        return Meta.getModels().$promise;
+        return Meta.getModels().$promise.then(function (res) {
+          return res;
+        });
+      }]
+    }
+  }).state('app.browser.models.info', {
+    url: '/:modelName/info',
+    templateUrl: 'modules/browser/views/models.info.html',
+    controllerAs: 'info',
+    controller: ['model', function (model) {
+      this.model = model;
+    }],
+    resolve: {
+      model: ['$stateParams', 'Meta', function ($stateParams, Meta) {
+        return Meta.getModelById({
+          name: $stateParams.modelName
+        }).$promise;
       }]
     }
   }).state('app.browser.models.items', {
@@ -47,18 +63,52 @@ app.config(function ($stateProvider) {
         }
       }]
     }
-  })
-    .state('app.browser.models.items.view', {
-      url: '/:modelId/view',
-      templateUrl: 'modules/browser/views/models.items.view.html',
-      controllerAs: 'view',
-      controller: ['item', function (item) {
+  }).state('app.browser.models.items.view', {
+    url: '/:modelId/view',
+    templateUrl: 'modules/browser/views/models.items.view.html',
+    controllerAs: 'view',
+    controller: ['item', function (item) {
+      this.item = item;
+      this.itemKeys = Object.keys(this.item);
+    }],
+    resolve: {
+      item: ['$stateParams', '$injector', function ($stateParams, $injector) {
+        var Model = $injector.get($stateParams.modelName);
+        if (typeof Model.find !== 'function') {
+          return false;
+        } else {
+          return Model.findOne({
+            filter: {
+              where: {
+                id: $stateParams.modelId
+              }
+            }
+          }).$promise;
+        }
+      }]
+    }
+  }).state('app.browser.models.items.edit', {
+    url: '/:modelId/edit',
+    templateUrl: 'modules/browser/views/models.items.edit.html',
+    controllerAs: 'edit',
+    controller: ['$state', 'SweetAlert', 'Model', 'model', 'item', 'itemFields',
+      function ($state, SweetAlert, Model, model, item, itemFields) {
         this.item = item;
-        this.itemKeys = Object.keys(this.item);
+        this.itemFields = itemFields;
+        this.submit = function () {
+          Model.upsert(this.item).$promise.then(function (res) {
+            SweetAlert.swal('Saved!', 'The item is saved.', 'success');
+            $state.go('app.browser.models.items.view', {modelName: model.name, modelId: res.id}, {reload: true});
+          });
+        };
       }],
-      resolve: {
-        item: ['$stateParams', '$injector', function ($stateParams, $injector) {
-          var Model = $injector.get($stateParams.modelName);
+    resolve: {
+      Model: ['$stateParams', '$injector',
+        function ($stateParams, $injector) {
+          return $injector.get($stateParams.modelName);
+        }],
+      item: ['$stateParams', 'Model',
+        function ($stateParams, Model) {
           if (typeof Model.find !== 'function') {
             return false;
           } else {
@@ -70,83 +120,9 @@ app.config(function ($stateProvider) {
               }
             }).$promise;
           }
-        }]
-      }
-    })
-    .state('app.browser.models.items.edit', {
-      url: '/:modelId/edit',
-      templateUrl: 'modules/browser/views/models.items.edit.html',
-      controllerAs: 'edit',
-      controller: ['$state', 'SweetAlert', 'Model', 'model', 'item', 'itemFields',
-        function ($state, SweetAlert, Model, model, item, itemFields) {
-          this.item = item;
-          this.itemFields = itemFields;
-          this.submit = function () {
-            Model.upsert(this.item).$promise.then(function (res) {
-              SweetAlert.swal('Saved!', 'The item is saved.', 'success');
-              $state.go('app.browser.models.items.view', {modelName: model.name, modelId: res.id}, {reload: true});
-            });
-          };
         }],
-      resolve: {
-        Model: ['$stateParams', '$injector',
-          function ($stateParams, $injector) {
-            return $injector.get($stateParams.modelName);
-          }],
-        item: ['$stateParams', 'Model',
-          function ($stateParams, Model) {
-            if (typeof Model.find !== 'function') {
-              return false;
-            } else {
-              return Model.findOne({
-                filter: {
-                  where: {
-                    id: $stateParams.modelId
-                  }
-                }
-              }).$promise;
-            }
-          }],
-        itemFields: ['model',
-          function (model) {
-            var result = [];
-            angular.forEach(model.properties, function (value, property) {
-              if (property !== 'id') {
-                var itemField = {
-                  key: property,
-                  type: 'input',
-                  templateOptions: {
-                    label: property
-                  }
-                };
-                result.push(itemField);
-              }
-            });
-            return result;
-          }]
-      }
-    })
-    .state('app.browser.models.items.add', {
-      url: '/add',
-      templateUrl: 'modules/browser/views/models.items.add.html',
-      controllerAs: 'add',
-      controller: ['$state', 'SweetAlert', 'Model', 'model', 'itemFields',
-        function ($state, SweetAlert, Model, model, itemFields) {
-          this.item = {};
-          this.itemFields = itemFields;
-          this.submit = function () {
-            Model.upsert(this.item).$promise.then(function (res) {
-              SweetAlert.swal('Saved!', 'The item is saved.', 'success');
-              $state.go('app.browser.models.items.view', {modelName: model.name, modelId: res.id}, {reload: true});
-            });
-          };
-        }],
-      resolve: {
-        Model: ['$stateParams', '$injector',
-          function ($stateParams, $injector) {
-            return $injector.get($stateParams.modelName);
-          }],
-        itemFields: ['model', function (model) {
+      itemFields: ['model',
+        function (model) {
           var result = [];
           angular.forEach(model.properties, function (value, property) {
             if (property !== 'id') {
@@ -162,57 +138,92 @@ app.config(function ($stateProvider) {
           });
           return result;
         }]
-      }
-    })
-    .state('app.browser.models.items.delete', {
-      url: '/:modelId/delete',
-      template: '',
-      controller: ['$state', 'SweetAlert', 'Model', 'model', 'item',
-        function ($state, SweetAlert, Model, model, item) {
-          SweetAlert.swal({
-              title: 'Are you sure?',
-              text: 'Your will not be able to recover this item!',
-              type: 'warning',
-              showCancelButton: true,
-              confirmButtonColor: '#DD6B55', confirmButtonText: 'Yes',
-              cancelButtonText: 'No',
-              closeOnConfirm: false,
-              closeOnCancel: true
-            },
-            function (isConfirm) {
-              if (isConfirm) {
-                Model.deleteById({id: item.id}).$promise.then(function () {
-                  SweetAlert.swal('Deleted!', 'The item is deleted.', 'success');
-                  $state.go('app.browser.models.items', {modelName: model.name}, {reload: true});
-                });
-              } else {
-                $state.go('app.browser.models.items.view', {modelName: model.name, modelId: item.id}, {reload: true});
+    }
+  }).state('app.browser.models.items.add', {
+    url: '/add',
+    templateUrl: 'modules/browser/views/models.items.add.html',
+    controllerAs: 'add',
+    controller: ['$state', 'SweetAlert', 'Model', 'model', 'itemFields',
+      function ($state, SweetAlert, Model, model, itemFields) {
+        this.item = {};
+        this.itemFields = itemFields;
+        this.submit = function () {
+          Model.upsert(this.item).$promise.then(function (res) {
+            SweetAlert.swal('Saved!', 'The item is saved.', 'success');
+            $state.go('app.browser.models.items.view', {modelName: model.name, modelId: res.id}, {reload: true});
+          });
+        };
+      }],
+    resolve: {
+      Model: ['$stateParams', '$injector',
+        function ($stateParams, $injector) {
+          return $injector.get($stateParams.modelName);
+        }],
+      itemFields: ['model', function (model) {
+        var result = [];
+        angular.forEach(model.properties, function (value, property) {
+          if (property !== 'id') {
+            var itemField = {
+              key: property,
+              type: 'input',
+              templateOptions: {
+                label: property
               }
-            }
-          );
-        }
-      ],
-      resolve: {
-        Model: ['$stateParams', '$injector',
-          function ($stateParams, $injector) {
-            return $injector.get($stateParams.modelName);
-          }],
-        item: ['$stateParams', 'Model',
-          function ($stateParams, Model) {
-            if (typeof Model.find !== 'function') {
-              return false;
+            };
+            result.push(itemField);
+          }
+        });
+        return result;
+      }]
+    }
+  }).state('app.browser.models.items.delete', {
+    url: '/:modelId/delete',
+    template: '',
+    controller: ['$state', 'SweetAlert', 'Model', 'model', 'item',
+      function ($state, SweetAlert, Model, model, item) {
+        SweetAlert.swal({
+            title: 'Are you sure?',
+            text: 'Your will not be able to recover this item!',
+            type: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#DD6B55', confirmButtonText: 'Yes',
+            cancelButtonText: 'No',
+            closeOnConfirm: false,
+            closeOnCancel: true
+          },
+          function (isConfirm) {
+            if (isConfirm) {
+              Model.deleteById({id: item.id}).$promise.then(function () {
+                SweetAlert.swal('Deleted!', 'The item is deleted.', 'success');
+                $state.go('app.browser.models.items', {modelName: model.name}, {reload: true});
+              });
             } else {
-              return Model.findOne({
-                filter: {
-                  where: {
-                    id: $stateParams.modelId
-                  }
-                }
-              }).$promise;
+              $state.go('app.browser.models.items.view', {modelName: model.name, modelId: item.id}, {reload: true});
             }
           }
-        ]
+        );
       }
+    ],
+    resolve: {
+      Model: ['$stateParams', '$injector',
+        function ($stateParams, $injector) {
+          return $injector.get($stateParams.modelName);
+        }],
+      item: ['$stateParams', 'Model',
+        function ($stateParams, Model) {
+          if (typeof Model.find !== 'function') {
+            return false;
+          } else {
+            return Model.findOne({
+              filter: {
+                where: {
+                  id: $stateParams.modelId
+                }
+              }
+            }).$promise;
+          }
+        }
+      ]
     }
-  );
+  });
 });
