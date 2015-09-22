b64dabf8b462be8fea0d7a36ae35b54eebc69aed
Bram Borggreve <borggreve@gmail.com>
2015-09-22 02:06:14 +0200

    Add mocha and a dummy test

---
+++ test/dummy.test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('test', function () {
+
+  describe('dummy', function () {
+    it('should pass this dummy test', function (done) {
+      done();
+    });
+  });
+
+
+});
