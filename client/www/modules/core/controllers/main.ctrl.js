9c18bde1e476b1268877dc81b2c2f96734d78c30
Willian Ribeiro Angelo <agfoccus@gmail.com>
2015-09-30 18:04:51 -0300

    *Merge master

--- client/www/modules/core/controllers/main.ctrl.js
+++ client/www/modules/core/controllers/main.ctrl.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
   'use strict';
   /**
    * @ngdoc function
@@ -13,16 +13,16 @@
    **/
   angular
     .module('com.module.core')
-    .controller('MainCtrl', function ($scope, $rootScope, $state, AppAuth, CoreService, User, gettextCatalog) {
-      AppAuth.ensureHasCurrentUser(function () {
+    .controller('MainCtrl', function($scope, $rootScope, $state, AppAuth, CoreService, User, gettextCatalog) {
+      AppAuth.ensureHasCurrentUser(function() {
         //This call also serves to redirect a user to the login screen, via the interceptor in users.auth.js, if they are not authenticated.
         $scope.currentUser = User.getCurrent();
       });
 
       $scope.menuoptions = $rootScope.menu;
 
-      $scope.logout = function () {
-        AppAuth.logout(function () {
+      $scope.logout = function() {
+        AppAuth.logout(function() {
           CoreService.toastSuccess(gettextCatalog.getString('Logged out'),
             gettextCatalog.getString('You are logged out!'));
           $state.go('login');
