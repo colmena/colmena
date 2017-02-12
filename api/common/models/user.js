fb5f029b3bd3ed5a287a8716dde8cc2f7cab7b73
Bram Borggreve <borggreve@gmail.com>
2017-02-11 22:46:11 -0500

    Add Info method to User model

--- api/common/models/user.js
+++ api/common/models/user.js
@@ -4,6 +4,9 @@ const config = require('config')
 
 module.exports = function (User) {
 
+  // This does not work on bulk create: https://github.com/strongloop/loopback-datasource-juggler/issues/793
+  User.validatesUniquenessOf('email', { scopedTo: [ 'realm' ], message: 'Email is not unique within this realm' })
+
   function getRegistrationEnabled () {
     return (config.has('settings.registrationEnabled') && config.get('settings.registrationEnabled') !== false)
   }
@@ -35,4 +38,40 @@ module.exports = function (User) {
     next()
   })
 
+  /**
+   * Method that retrieves all the roles in the system
+   * @returns boolean
+   */
+  User.getAllRoleNames = function getAllRoleNames() {
+    return User.app.models.Role
+      .find()
+      .then(roles => roles.map(role => role.name))
+  }
+
+  /**
+   * Get the roles for this User.
+   * @returns {Object} A map of all roles of this user
+   */
+  User.prototype.info = function info() {
+    const result = {
+      user: this,
+      roles: {},
+    }
+    let allRoleNames = []
+    let userRoleNames = []
+
+    return this.roles
+      .getAsync()
+      .then(userRoles => userRoles.map(userRole => userRole.name))
+      .then(res => (userRoleNames = res))
+      .then(() => User.getAllRoleNames())
+      .then(res => (allRoleNames = res))
+      .then(() => (
+        result.roles = {
+          assigned: allRoleNames.filter(name => userRoleNames.indexOf(name) !== -1),
+          unassigned: allRoleNames.filter(name => userRoleNames.indexOf(name) === -1),
+        }
+      ))
+      .then(() => result)
+  }
 }
