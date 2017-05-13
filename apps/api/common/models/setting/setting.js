b358daf2863520f6a2f8ada17d6acff9c82371cf
Bram Borggreve <borggreve@gmail.com>
2017-05-12 23:41:41 -0500

    fix(api): make sure Settings get exposed with correct type [Fixes #238]

---
+++ apps/api/common/models/setting/setting.js
@@ -0,0 +1,20 @@
+'use strict'
+
+
+module.exports = function (Setting) {
+
+  Setting.observe('loaded', (ctx, next) => {
+    if (ctx.instance && ctx.instance.type) {
+      switch (ctx.instance.type) {
+        case 'boolean':
+          ctx.instance.value = Boolean(String(ctx.instance.value) === 'true')
+          break
+        case 'integer':
+          ctx.instance.value = parseInt(ctx.instance.value)
+          break
+      }
+    }
+    return next()
+  })
+
+}
