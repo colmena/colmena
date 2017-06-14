edccb37dff85c8a9858140e2333b8932704ae46c
Bram Borggreve <borggreve@gmail.com>
2017-06-14 04:56:10 -0500

    feat(logger): add support for logging in color with chalk

--- packages/logger/index.js
+++ packages/logger/index.js
@@ -1,6 +1,9 @@
 const bunyan = require('bunyan')
 const bunyanFormat = require('bunyan-format')
 
+const chalk = require('chalk')
+chalk.enabled = true
+
 const name = process.env.API_LOG_NAME || 'colmena'
 const level = process.env.API_LOG_LEVEL || 'debug'
 
@@ -15,5 +18,11 @@ const log = bunyan.createLogger({
   name,
   streams,
 })
+
+const colors = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray']
+
+colors.forEach(color => log[color] = (...str) => log.info(chalk[color](...str)))
+colors.forEach(color => log[color].b = (...str) => log.info(chalk[color].bold(...str)))
+colors.forEach(color => log[color].d = (...str) => log.info(chalk[color].dim(...str)))
 
 module.exports = log
