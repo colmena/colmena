1e361741ee8b0566938dbfb484f67227be52828a
Bram Borggreve <borggreve@gmail.com>
2017-06-15 03:00:06 -0500

    feat(api): control INITDB and MIGRATEDB separately

--- apps/api/server/boot/99-db-migrate-update.js
+++ apps/api/server/boot/99-db-migrate-update.js
@@ -7,8 +7,8 @@ const { dbMigrate, dbUpdate } = require('@colmena/api-helpers')
 module.exports = function(app, cb) {
 
   // Check if there is user configured Settings
-  if (!config.has('system.initdb') || config.get('system.initdb') === false) {
-    log.gray.b('[db-migrate] skipping database migration (initdb = false)')
+  if (!config.has('system.migratedb') || config.get('system.migratedb') === false) {
+    log.gray.b('[db-migrate] skipping database migration (migratedb = false)')
     return cb()
   }
 
