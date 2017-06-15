a9bdbd03865a3188bf5f0edf960fcbb804bec0d8
Bram Borggreve <borggreve@gmail.com>
2017-06-15 05:40:20 -0500

    feat(api): add computed field username

--- modules/api-system/common/models/system-user.js
+++ modules/api-system/common/models/system-user.js
@@ -1,12 +1,14 @@
 'use strict';
 const crypto = require('crypto');
 
+const getHash = str => crypto.createHash('md5').update(str).digest("hex")
+const getGravatarUrl = email => `http://www.gravatar.com/avatar/${getHash(email)}?s=120`
+
 module.exports = function(SystemUser) {
 
-  SystemUser.getAvatarUrl = (item) => {
-    const hash = crypto.createHash('md5').update(item.email).digest("hex")
-    return `http://www.gravatar.com/avatar/${hash}?s=120`
-  }
+  SystemUser.getAvatarUrl = item => getGravatarUrl(item.email)
+
+  SystemUser.getFullName = item => `${item.firstName} ${item.lastName}`
 
   SystemUser.observe('before save', (ctx, next) => {
     if (ctx.isNewInstance && !ctx.instance.username && ctx.instance.email) {
