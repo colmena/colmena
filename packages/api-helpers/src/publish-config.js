795b9720c3aa8f8ece3f40b510ea044e13de1630
Bram Borggreve <borggreve@gmail.com>
2017-06-16 09:54:21 -0500

    feat(api-helpers): move publish config middleware to api-helpers

--- packages/api-helpers/src/publish-config.js
+++ packages/api-helpers/src/publish-config.js
@@ -2,11 +2,12 @@ const config = require('config')
 
 function createConfig() {
   const result = {
+    nodeEnv: process.env.NODE_ENV || 'development',
     baseUrl: config.get('api.baseUrl'),
     version: config.get('api.version'),
-    url: config.get('api.url'),
-    nodeEnv: process.env.NODE_ENV || 'development',
   }
+
+  result.url = [result.baseUrl.replace(/\/$/, ''), result.version].join('/')
 
   return `window.apiConfig = ${JSON.stringify(result)}`
 }
