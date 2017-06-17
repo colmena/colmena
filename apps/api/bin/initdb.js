a6bc9d7090bff6381dc72ceb78ec1716e4481e77
Bram Borggreve <borggreve@gmail.com>
2017-06-17 07:38:58 -0500

    refactor(api): remove unneeded variable

--- apps/api/bin/initdb.js
+++ apps/api/bin/initdb.js
@@ -3,6 +3,4 @@
 process.env.INITDB = true
 process.env.MIGRATEDB = true
 
-const app = require('../server/server')
-
-app.on('booted', process.exit)
+require('../server/server').on('booted', process.exit)
