5118e623d400c4594bc327e48f0670d2a5959887
Bram Borggreve <borggreve@gmail.com>
2017-06-17 12:13:36 -0500

    feat(api-system): only use gravatar if avatar field is empty

--- modules/api-system/common/models/system-user/user.computed.js
+++ modules/api-system/common/models/system-user/user.computed.js
@@ -3,7 +3,8 @@
 const { getGravatarUrl } = require('@colmena/api-helpers/src/gravatar')
 
 module.exports = function(SystemUser) {
-  SystemUser.getAvatarUrl = item => getGravatarUrl(item.email)
+  SystemUser.getAvatarUrl = item =>
+    item.avatar ? item.avatar : getGravatarUrl(item.email)
 
   SystemUser.getFullName = item => `${item.firstName} ${item.lastName}`
 }
