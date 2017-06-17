546ad843ba51fae235e7110c41b090d1c838b813
Bram Borggreve <borggreve@gmail.com>
2017-06-17 07:56:37 -0500

    feat(api-system): clean up user registration permission config

--- modules/api-system/common/models/system-user/user.registration.js
+++ modules/api-system/common/models/system-user/user.registration.js
@@ -4,30 +4,26 @@ const config = require('config')
 const log = require('@colmena/logger')
 
 module.exports = function(SystemUser) {
-  function getRegistrationEnabled() {
-    return (
-      config.has('settings.registrationEnabled') &&
-      config.get('settings.registrationEnabled') !== false
-    )
-  }
+  const isRegistrationEnabled = () =>
+    config.has('settings.registrationEnabled') &&
+    config.get('settings.registrationEnabled') !== false
 
-  function configureUserRegistration() {
-    const enabled = getRegistrationEnabled()
-
-    if (enabled) {
-      log.cyan.b('[api-system] User registration enabled')
-    } else {
-      log.cyan.b('[api-system] User registration disabled')
-    }
-
-    // FIXME: I wish there was a way to do this outside of the Model (like a boot script)
+  const setCreateAcl = enabled =>
     SystemUser.settings.acls.push({
       principalType: 'ROLE',
       principalId: '$unauthenticated',
       permission: enabled ? 'ALLOW' : 'DENY',
       property: 'create',
     })
+
+  const configureRegistration = () => {
+    if (isRegistrationEnabled()) {
+      log.cyan.b('[system-user] User registration enabled')
+      return setCreateAcl(true)
+    }
+    log.cyan.b('[system-user] User registration disabled')
+    return setCreateAcl(false)
   }
 
-  configureUserRegistration()
+  SystemUser.app.once('booted', () => configureRegistration())
 }
