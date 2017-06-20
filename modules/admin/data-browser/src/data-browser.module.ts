ce24f68beccf3c64de1dd0d2b883597b10642abf
Bram Borggreve <borggreve@gmail.com>
2017-06-20 03:06:12 -0500

    refactor(admin-data-browser): clean up module imports

--- modules/admin/data-browser/src/data-browser.module.ts
+++ modules/admin/data-browser/src/data-browser.module.ts
@@ -1,23 +1,17 @@
 import { NgModule } from '@angular/core'
-import { CommonModule } from '@angular/common'
-import { RouterModule } from '@angular/router'
-
 import { ColmenaUiModule } from '@colmena/admin-ui'
 
 import { BrowserRoutingModule } from './data-browser-routing.module';
+
+import { DataBrowserApi } from './data-browser.service'
 
 import { BrowserComponent } from './browser/browser.component'
 import { BrowserItemComponent } from './browser/browser-item.component'
 import { BrowserItemsComponent } from './browser/browser-items.component'
 import { BrowserListComponent } from './browser/browser-list.component'
 
-import { DataBrowserApi } from './data-browser.service'
-
-
 @NgModule({
   imports: [
-    CommonModule,
-    RouterModule,
     ColmenaUiModule,
     BrowserRoutingModule,
   ],
