3b9bd3c14aeb6220bf2cf439687f5b28d9c3c24f
Brannon N. Darby II <bdarby86@gmail.com>
2017-06-20 02:52:15 -0400

    fix(admin-storage): added missing edit action

--- modules/admin-storage/src/components/files.component.ts
+++ modules/admin-storage/src/components/files.component.ts
@@ -14,38 +14,39 @@ import { StorageService } from '../storage.service'
   `,
 })
 export class FilesComponent {
-
   @ViewChild('grid') private grid
   @ViewChild('view') private view
 
   public item: any = {}
 
-  constructor(
-    public service: StorageService,
-    public uiService: UiService,
-  ) {
+  constructor(public service: StorageService, public uiService: UiService) {
     this.service.domain = { id: 'default' }
   }
 
   action(event) {
     switch (event.action) {
+      case 'edit':
       case 'view':
         this.item = event.item
         this.view.title = `${this.item.name}`
         this.view.show()
         break
       case 'delete':
-        const successCb = () => this.service
-          .deleteItem(event.item,
+        const successCb = () =>
+          this.service.deleteItem(
+            event.item,
             () => this.grid.refreshData(),
-            (err) => this.uiService.toastError('Error deleting item', err.message))
-        const question = { title: 'Are you sure?', text: 'The action can not be undone.' }
-        this.uiService.alertQuestion( question, successCb, () => ({}) )
+            err => this.uiService.toastError('Error deleting item', err.message)
+          )
+        const question = {
+          title: 'Are you sure?',
+          text: 'The action can not be undone.',
+        }
+        this.uiService.alertQuestion(question, successCb, () => ({}))
         break
       default:
         console.log('Unknown event action', event)
         break
     }
   }
-
 }
