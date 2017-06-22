9aec6535f0ea7f5c4ee9a2c633f6ac12788617d9
Brannon N. Darby II <bdarby86@gmail.com>
2017-06-21 22:25:18 -0400

    feat(admin-dashboard): add password form in dashboard

--- modules/admin/dashboard/src/components/password.component.ts
+++ modules/admin/dashboard/src/components/password.component.ts
@@ -1,35 +1,71 @@
 import { get } from 'lodash'
 import { Component } from '@angular/core'
+import { UiService, FormService } from '@colmena/admin-ui'
+import { SystemUserApi } from '@colmena/admin-lb-sdk'
 import { Store } from '@ngrx/store'
 
 @Component({
   selector: 'app-password',
   template: `
-    <div class="row">
-      <div class="col-md-6 offset-md-3">
-        <ui-card>
-          <ui-card-header>
-            <i class="icon-key"></i> Password
-          </ui-card-header>
-          <ui-card-content>
-            <pre>{{user | json}}</pre>
-          </ui-card-content>
-        </ui-card>
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-md-6 offset-md-3 mb-1">
+        <h5>Change Password</h5>
+        <hr />
+        <ui-form [config]="formConfig"
+                 [item]="user"
+                 (action)="handleAction($event)">
+        </ui-form>
       </div>
     </div>
+  </div>
   `,
 })
 export class PasswordComponent {
-
   public user = {}
-  constructor(
-    private store: Store<any>,
-  ) {
-    this.store
-      .select('auth')
-      .subscribe((res: any) => {
-        this.user = get(res, 'currentUser.user')
-      })
+  public formConfig: any = {
+    hasHeader: false,
+    fields: [],
+    showCancel: false,
   }
 
+  constructor(
+    public service: SystemUserApi,
+    private store: Store<any>,
+    public uiService: UiService,
+    private formService: FormService
+  ) {
+    this.store.select('auth').subscribe((res: any) => {
+      this.user = get(res, 'currentUser.user')
+    })
+    this.formConfig.fields = [
+      this.formService.password('password', {
+        label: 'Password',
+        placeholder: 'Must be at least 5 characters',
+      }),
+      this.formService.password('password', {
+        label: 'Confirm Password',
+        placeholder: 'Re-enter the password to confirm',
+      }),
+    ]
+  }
+
+  handleAction(event) {
+    switch (event.action) {
+      case 'save':
+        return this.service
+          .resetPassword(Object.assign(this.user, event.item))
+          .subscribe(
+            res =>
+              this.uiService.toastSuccess(
+                'Change Password Success',
+                `Your password has been changed successfully`
+              ),
+            err =>
+              this.uiService.toastError('Change Password Fail', err.message)
+          )
+      default:
+        return console.log('Unknown Event Action', event)
+    }
+  }
 }
