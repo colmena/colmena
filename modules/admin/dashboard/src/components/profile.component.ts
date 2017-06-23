6f99aaa899693a590143e25c8e60778926c444f8
Brannon N. Darby II <bdarby86@gmail.com>
2017-06-23 17:37:07 -0400

    refactor(admin-dashboard): add profile edit form to dashboard

--- modules/admin/dashboard/src/components/profile.component.ts
+++ modules/admin/dashboard/src/components/profile.component.ts
@@ -1,35 +1,88 @@
 import { get } from 'lodash'
 import { Component } from '@angular/core'
 import { Store } from '@ngrx/store'
+import { SystemUser, SystemUserApi } from '@colmena/admin-lb-sdk'
+import { UiService, FormService } from '@colmena/admin-ui'
 
 @Component({
   selector: 'app-profile',
   template: `
+  <div class="container">
     <div class="row">
-      <div class="col-md-6 offset-md-3">
-        <ui-card>
-          <ui-card-header>
-            <i class="icon-user"></i> Profile
-          </ui-card-header>
-          <ui-card-content>
-            <pre>{{user | json}}</pre>
-          </ui-card-content>
-        </ui-card>
+      <div class="col-md-4">
+        <img *ngIf="user"
+             [src]="user.avatar"
+             class="img-fluid rounded float-left mb-2"
+             width="200px" />
+      </div>
+      <div class="col-md-8">
+        <h3>{{ user.firstName }} {{ user.lastName }}</h3>
+        <hr />
+        <span class="float-right lead">{{ user.email }}</span>
+      </div>
+      <div class="col-md-12">
+        <ui-form *ngIf="user"
+                 [config]="formConfig"
+                 [item]="user"
+                 (action)="handleAction($event)">
+        </ui-form>
       </div>
     </div>
+  </div>
   `,
 })
 export class ProfileComponent {
+  public user: SystemUser
+  public formConfig: any = {}
 
-  public user = {}
   constructor(
     private store: Store<any>,
+    private service: SystemUserApi,
+    public uiService: UiService,
+    public formService: FormService
   ) {
-    this.store
-      .select('auth')
-      .subscribe((res: any) => {
-        this.user = get(res, 'currentUser.user')
-      })
+    this.store.select('auth').subscribe((res: any) => {
+      this.user = get(res, 'currentUser.user')
+      this.formConfig = {
+        icon: 'fa fa-user',
+        fields: [
+          this.formService.input('firstName', {
+            label: 'First name',
+            placeholder: 'First name',
+          }),
+          this.formService.input('lastName', {
+            label: 'Last name',
+            placeholder: 'Last name',
+          }),
+          this.formService.email('email', {
+            label: 'Email address',
+            placeholder: 'Email address',
+          }),
+        ],
+        showCancel: false,
+        hasHeader: false,
+      }
+    })
   }
 
+  handleAction(event) {
+    switch (event.action) {
+      case 'save':
+        return this.service.upsertItem(
+          event.user,
+          () => {
+            this.uiService.toastSuccess(
+              'Update Profile Success',
+              `<u>${event.user.username}</u>'s profile has been ${event.user.id
+                ? 'created'
+                : 'updated '} successfully'`
+            )
+            this.handleAction({ action: 'cancel' })
+          },
+          err => this.uiService.toastError('Update Profile Fail', err.message)
+        )
+      default:
+        return console.log('Unknown Event Action:', event)
+    }
+  }
 }
