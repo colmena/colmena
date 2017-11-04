075a4b95a00e3e7cfe574ca31d6e52701dc1f929
Bram Borggreve <borggreve@gmail.com>
2017-11-05 00:01:28 +0700

    feat(api-system): assign system-user role to newly created users

--- modules/api/system/common/models/system-user/user.observers.js
+++ modules/api/system/common/models/system-user/user.observers.js
@@ -1,10 +1,20 @@
 'use strict'
 
 module.exports = function(SystemUser) {
+  // Set the username property to email
   SystemUser.observe('before save', (ctx, next) => {
     if (ctx.isNewInstance && !ctx.instance.username && ctx.instance.email) {
       ctx.instance.username = ctx.instance.email
     }
     next()
   })
+
+  // Assign the system-user role to each new user
+  SystemUser.observe('before save', (ctx, next) => {
+    if (ctx.isNewInstance && ctx.instance) {
+      ctx.instance.addRole('system-user')
+    }
+    next()
+  })
+
 }
