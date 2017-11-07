1bbcc57c97f64e04f511c38c27feb801ac02ea01
Caster Zeng <caster_zeng@wushuu.com>
2017-11-07 11:30:47 +0800

    fix(api-helpers): getGravatarUrl() Error when POST /api/v1/Users/reset-password

--- packages/api-helpers/src/gravatar.js
+++ packages/api-helpers/src/gravatar.js
@@ -5,7 +5,6 @@ const getHash = str => crypto.createHash('md5').update(str).digest('hex')
 const gravatarUrl = 'http://www.gravatar.com/avatar/'
 const gravatarSize = 150
 
-const getGravatarUrl = email =>
-  `${gravatarUrl}${getHash(email)}?s=${gravatarSize}`
+const getGravatarUrl = (email = '') => `${gravatarUrl}${getHash(email)}?s=${gravatarSize}`
 
 module.exports = { getGravatarUrl }
